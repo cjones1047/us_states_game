c3fb017458ed93911ad54e7eb42ba29123df4861
cjones1047 <cjmj1047@gmail.com>
2022-12-06 06:39:11 -0600

    Screen keeps receiving text input while running

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("US States Game")
@@ -6,4 +7,12 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-screen.exitonclick()
+game_is_on = True
+game_title = "Guess the State"
+correct_guesses = 0
+
+while game_is_on:
+    answer_state = screen.textinput(title=game_title, prompt="What's another state's name?").lower()
+    print(answer_state)
+
+turtle.mainloop()
