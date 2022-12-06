8123ce00b3adff2380e2b21efe61652ea4e511ff
cjones1047 <cjmj1047@gmail.com>
2022-12-06 07:57:49 -0600

    Upon user entering 'exit', game is stopped and all missed states are written to a new dataframe in a new csv named 'missed_states.csv'

--- main.py
+++ main.py
@@ -20,9 +20,29 @@ df = pandas.read_csv("50_states.csv")
 while len(correct_guesses) < 50:
     answer_state = screen.textinput(title=game_title, prompt="What's another state's name?").title()
     state_row = df[df["state"] == answer_state]
+    if answer_state == "Exit":
+        break
     if len(state_row) > 0:
         state_name = state_row.state.item()
         new_state.goto(state_row.x.item(), state_row.y.item())
         new_state.write(state_name, align="left", font=("Arial", 8, "normal"))
         correct_guesses.append(state_name)
         game_title = f"{len(correct_guesses)}/50 States Correct"
+
+# write states not guessed to csv
+missed_states = {
+    "state": [],
+    "x": [],
+    "y": []
+}
+
+for state in df["state"]:
+    if state not in correct_guesses:
+        x_cor = df[df["state"] == state].x.item()
+        y_cor = df[df["state"] == state].y.item()
+        missed_states["state"].append(state)
+        missed_states["x"].append(x_cor)
+        missed_states["y"].append(y_cor)
+
+missed_states_df = pandas.DataFrame(missed_states)
+missed_states_df.to_csv("missed_states.csv")
