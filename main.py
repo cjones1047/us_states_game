23fb7fd360fe8820429476ca138cd08c9bcc2e50
cjones1047 <cjmj1047@gmail.com>
2022-12-06 07:24:55 -0600

    Correct guess is added to list, number of correct guesses out of 50 total states kept track of via text input title

--- main.py
+++ main.py
@@ -11,14 +11,19 @@ game_is_on = True
 game_title = "Guess the State"
 correct_guesses = []
 
+new_state = turtle.Turtle()
+new_state.penup()
+new_state.hideturtle()
+new_state.speed("normal")
+
 df = pandas.read_csv("50_states.csv")
 
 while game_is_on:
     answer_state = screen.textinput(title=game_title, prompt="What's another state's name?").title()
     state_row = df[df["state"] == answer_state]
     if len(state_row) > 0:
-        new_state = turtle.Turtle()
-        new_state.hideturtle()
-        new_state.penup()
+        state_name = state_row.state.item()
         new_state.goto(state_row.x.item(), state_row.y.item())
-        new_state.write(state_row.state.item(), align="left", font=("Arial", 8, "normal"))
+        new_state.write(state_name, align="left", font=("Arial", 8, "normal"))
+        correct_guesses.append(state_name)
+        game_title = f"{len(correct_guesses)}/50 States Correct"
